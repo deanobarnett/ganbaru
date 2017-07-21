f9dcfd6ea3da41989a51a16b27c402a08a44928e
Dean Barnett <dean@shopkeep.com>
2017-07-21 11:08:19 +0100

    Shuffle option

--- lib/ganbaru/leader.rb
+++ lib/ganbaru/leader.rb
@@ -8,17 +8,16 @@ module Ganbaru
       @queue = queue
     end
 
-    def run(dir)
+    def run(dir, shuffle: false)
       puts "Test ID: #{@queue.id}"
       puts "Scanning #{dir}..."
 
       spec_files = Dir.glob("#{dir}/**/*_spec.rb")
-      puts "There are #{spec_files.size} files."
-
       if spec_files.empty?
         puts "No spec files found under '#{dir}'."
         0
       else
+        spec_files = shuffle ? spec_files.shuffle : spec_files
         @queue.push(spec_files).tap do
           puts "Loaded #{spec_files.size} tests to run."
         end
